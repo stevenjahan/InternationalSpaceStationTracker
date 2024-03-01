8d9429c3ccb94fab21b2cd1d27e971e0da65d6e7
Steven <stevenjahan1992@gmail.com>
2024-03-01 15:42:23 -0600

    Day 2 Creating International Space Station project Controllers Getters Setters Handling errors calling third party API

--- src/main/java/com/farhat/steven/InternationalSpaceStation/InternationalSpaceStationApplication.java
+++ src/main/java/com/farhat/steven/InternationalSpaceStation/InternationalSpaceStationApplication.java
@@ -2,8 +2,10 @@ package com.farhat.steven.InternationalSpaceStation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class InternationalSpaceStationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/farhat/steven/InternationalSpaceStation/service/LocationService.java
+++ src/main/java/com/farhat/steven/InternationalSpaceStation/service/LocationService.java
@@ -2,15 +2,24 @@ package com.farhat.steven.InternationalSpaceStation.service;
 
 
 import com.farhat.steven.InternationalSpaceStation.models.IssLocator;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class LocationService {
-
+    private static final String FAILED = "failed";
+    @HystrixCommand(fallbackMethod = "fallback")
     public IssLocator getIssLocation() {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("http://api.open-notify.org/iss-now.jsonxxx", IssLocator.class);
+        return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", IssLocator.class);
+    }
+
+    public IssLocator fallback(){
+        IssLocator issLocator = new IssLocator();
+        issLocator.setMessage(FAILED);
+        return issLocator;
+
     }
 }
 
@@ -23,5 +32,4 @@ public class LocationService {
             issLocator.setMessage(FAILED);
             return issLocator;
         }
-
  */
