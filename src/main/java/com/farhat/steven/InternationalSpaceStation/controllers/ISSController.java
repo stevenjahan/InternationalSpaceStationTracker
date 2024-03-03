191a1dfd0cc3feb986c7c5cc3fa8fed897b54360
Steven <stevenjahan1992@gmail.com>
2024-03-02 19:59:56 -0600

    Day 3.5 Debugging ISSController, and updating HTML file to pinpoint ISS on the map using red dot and make allow it to move to track the station

--- src/main/java/com/farhat/steven/InternationalSpaceStation/controllers/ISSController.java
+++ src/main/java/com/farhat/steven/InternationalSpaceStation/controllers/ISSController.java
@@ -3,6 +3,7 @@ package com.farhat.steven.InternationalSpaceStation.controllers;
 import com.farhat.steven.InternationalSpaceStation.models.IssLocator;
 import com.farhat.steven.InternationalSpaceStation.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ISSController {
     @Autowired
     private LocationService locationService;
     @GetMapping("/issLocation")
+    @CrossOrigin
     public IssLocator getISSLocation() {
       return this.locationService.getIssLocation();
     }
