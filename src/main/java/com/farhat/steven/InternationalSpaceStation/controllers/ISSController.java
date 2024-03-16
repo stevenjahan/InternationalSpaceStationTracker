e0d48e090a54134f4584ce68d8eb5e6836bf5aa5
Steven <stevenjahan1992@gmail.com>
2024-03-15 22:52:32 -0500

    Day 5 Project Complete

--- src/main/java/com/farhat/steven/InternationalSpaceStation/controllers/ISSController.java
+++ src/main/java/com/farhat/steven/InternationalSpaceStation/controllers/ISSController.java
@@ -7,15 +7,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import static javax.management.remote.JMXConnectionNotification.FAILED;
 
 @RestController
 @RequestMapping("/api")
 public class ISSController {
-
-    private static final String FAILED = "failed";
 
 
     @Autowired
